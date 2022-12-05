6cf2d1050b2c1937dca9c8246951b6ecc27ef4a0
Olivier Belhomme <2453684+olb17@users.noreply.github.com>
2022-12-05 20:05:56 +0000

    String.split with trim

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,13 +1,14 @@
 defmodule AdventOfCode.Day01 do
   def part1(args) do
     args
-    |> String.split("\n")
-    |> Enum.map(&String.trim/1)
-    |> Enum.chunk_by(&(&1 == ""))
-    |> Enum.filter(&(&1 != [""]))
+    |> String.trim()
+    |> String.split("\n\n")
     |> Enum.map(fn lutin ->
       lutin
-      |> Enum.map(&String.to_integer/1)
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        line |> String.trim() |> String.to_integer()
+      end)
       |> Enum.sum()
     end)
     |> Enum.max()
@@ -15,11 +16,11 @@ defmodule AdventOfCode.Day01 do
 
   def part2(args) do
     args
+    |> String.trim()
     |> String.split("\n\n")
     |> Enum.map(fn lutin ->
       lutin
-      |> String.trim()
-      |> String.split("\n")
+      |> String.split("\n", trim: true)
       |> Enum.map(fn line ->
         line |> String.trim() |> String.to_integer()
       end)
