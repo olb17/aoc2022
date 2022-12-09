b10282a59f28eb2afaf1a535f285c1172ec48410
Olivier Belhomme <2453684+olb17@users.noreply.github.com>
2022-12-09 19:53:36 +0000

    day 7

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -1,37 +1,84 @@
 defmodule AdventOfCode.Day07 do
   def part1(args) do
-    {positions, min_candidate, max_candidate} = parse_args(args)
-
-    min_candidate..max_candidate
-    |> Enum.map(fn candidate ->
-      Enum.reduce(positions, 0, fn {k, v}, acc -> abs(k - candidate) * v + acc end)
-    end)
-    |> Enum.min()
+    args
+    |> String.split("\n", trim: true)
+    |> parse_tree()
+    |> compute_size("/")
+    |> Map.filter(fn {_k, v} -> v.size <= 100_000 end)
+    |> Enum.reduce(0, fn {_, dir}, acc -> dir.size + acc end)
   end
 
-  defp parse_args(args) do
-    positions =
-      args
-      |> String.split("\n", trim: true)
-      |> List.first()
-      |> String.split(",", trim: true)
-      |> Enum.map(&String.to_integer/1)
-      |> Enum.frequencies()
+  def parse_tree(lines) do
+    Enum.reduce(
+      lines,
+      {%{"/" => %{name: "/", parent: nil, size: 0, dirs: [], files: []}}, "/"},
+      fn
+        "$ cd /", {repo, "/"} ->
+          {repo, "/"}
 
-    {min_candidate, max_candidate} = Map.keys(positions) |> Enum.min_max()
-    {positions, min_candidate, max_candidate}
+        "$ cd ..", {repo, curr_dir} ->
+          {repo, repo[curr_dir].parent}
+
+        "$ cd " <> dir_name, {repo, curr_dir} ->
+          path = "#{repo[curr_dir].name}/#{dir_name}"
+
+          if Map.has_key?(repo, path) do
+            {repo, path}
+          else
+            new_dir = %{name: path, parent: repo[curr_dir].name, size: 0, dirs: [], files: []}
+            {Map.put(repo, path, new_dir), path}
+          end
+
+        "$ ls", {repo, curr_dir} ->
+          {repo, curr_dir}
+
+        "dir " <> dir_name, {repo, curr_dir} ->
+          new_path = "#{repo[curr_dir].name}/#{dir_name}"
+          new_dir = %{repo[curr_dir] | dirs: [new_path | repo[curr_dir].dirs]}
+          {%{repo | curr_dir => new_dir}, curr_dir}
+
+        line, {repo, curr_dir} ->
+          [file_size, file_name] = String.split(line, " ")
+
+          new_dir = %{
+            repo[curr_dir]
+            | files: [{String.to_integer(file_size), file_name} | repo[curr_dir].files]
+          }
+
+          {%{repo | curr_dir => new_dir}, curr_dir}
+      end
+    )
+    |> elem(0)
+  end
+
+  def compute_size(repo, curr_dir) do
+    file_size = repo[curr_dir].files |> Enum.map(&elem(&1, 0)) |> Enum.sum()
+
+    {repo, dir_size} =
+      Enum.reduce(repo[curr_dir].dirs, {repo, 0}, fn dir, {repo, size} ->
+        repo = compute_size(repo, dir)
+        {repo, size + repo[dir].size}
+      end)
+
+    new_dir = %{repo[curr_dir] | size: dir_size + file_size}
+
+    %{repo | curr_dir => new_dir}
   end
 
   def part2(args) do
-    {positions, min_candidate, max_candidate} = parse_args(args)
+    repo =
+      args
+      |> String.split("\n", trim: true)
+      |> parse_tree()
+      |> compute_size("/")
 
-    min_candidate..max_candidate
-    |> Enum.map(fn candidate ->
-      Enum.reduce(positions, 0, fn {k, v}, acc ->
-        (abs(k - candidate) + 1) * abs(k - candidate) / 2 * v + acc
-      end)
-    end)
-    |> Enum.min()
-    |> Kernel.trunc()
+    required_size = 30_000_000 - 70_000_000 + repo["/"].size
+
+    repo
+    |> Map.filter(fn {_k, v} -> v.size > required_size end)
+    |> Map.values()
+    |> Enum.sort(&(&1.size < &2.size))
+    |> hd()
+    |> Map.get(:size)
   end
 end
