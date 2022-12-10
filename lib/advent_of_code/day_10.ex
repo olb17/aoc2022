c6e553e2e4811468c40c80d2fd5207a692d504b9
Olivier Belhomme <2453684+olb17@users.noreply.github.com>
2022-12-10 22:19:45 +0000

    day 10

--- lib/advent_of_code/day_10.ex
+++ lib/advent_of_code/day_10.ex
@@ -1,54 +1,63 @@
 defmodule AdventOfCode.Day10 do
   def part1(args) do
+    instr = parse(args)
+
+    %{instr: instr, x: 1, cycle: 0, signal_strength: 0}
+    |> loop(&collect_ss/1)
+    |> Map.get(:signal_strength)
+  end
+
+  defp loop(%{instr: [{:addx, c, 0} | rest], x: x} = machine, part_fun) do
+    %{machine | instr: rest, x: x + c} |> loop(part_fun)
+  end
+
+  defp loop(%{instr: [{:addx, c, ttl} | rest], cycle: cycle} = machine, part_fun) do
+    %{machine | instr: [{:addx, c, ttl - 1} | rest], cycle: cycle + 1}
+    |> part_fun.()
+    |> loop(part_fun)
+  end
+
+  defp loop(%{instr: [:noop | rest], cycle: cycle} = machine, part_fun) do
+    %{machine | instr: rest, cycle: cycle + 1} |> part_fun.() |> loop(part_fun)
+  end
+
+  defp loop(%{instr: []} = machine, _part_fun), do: machine
+
+  defp collect_ss(%{x: x, cycle: cycle, signal_strength: ss} = machine)
+       when rem(cycle - 20, 40) == 0 do
+    %{machine | signal_strength: ss + x * cycle}
+  end
+
+  defp collect_ss(machine), do: machine
+
+  defp parse(args) do
     args
     |> String.split("\n", trim: true)
-    |> Enum.map(fn line ->
-      line
-      |> String.split("", trim: true)
-      |> score_line([], true)
+    |> Enum.map(fn
+      "addx " <> c -> {:addx, String.to_integer(c), 2}
+      "noop" -> :noop
     end)
-    |> Enum.sum()
   end
-
-  def score_line([], [], _), do: 0
-  def score_line([], _, true), do: 0
-  def score_line([], stack, false), do: {:incomplete, stack}
-
-  def score_line([char | rest], stack, flag) when char in ["(", "[", "{", "<"] do
-    score_line(rest, [char | stack], flag)
-  end
-
-  def score_line([")" | rest], ["(" | stack], flag), do: score_line(rest, stack, flag)
-  def score_line(["]" | rest], ["[" | stack], flag), do: score_line(rest, stack, flag)
-  def score_line(["}" | rest], ["{" | stack], flag), do: score_line(rest, stack, flag)
-  def score_line([">" | rest], ["<" | stack], flag), do: score_line(rest, stack, flag)
-
-  def score_line([")" | _rest], _, _), do: 3
-  def score_line(["]" | _rest], _, _), do: 57
-  def score_line(["}" | _rest], _, _), do: 1197
-  def score_line([">" | _rest], _, _), do: 25137
 
   def part2(args) do
-    resp =
-      args
-      |> String.split("\n", trim: true)
-      |> Enum.map(fn line -> String.split(line, "", trim: true) end)
-      |> Enum.map(fn line ->
-        case score_line(line, [], false) do
-          {:incomplete, stack} -> stack
-          _ -> nil
-        end
-      end)
-      |> Enum.filter(&(&1 != nil))
-      |> Enum.map(fn stack -> stack |> score_autocomplete(0) end)
-      |> Enum.sort()
+    IO.puts("")
+    instr = parse(args)
 
-    Enum.at(resp, div(Enum.count(resp), 2))
+    %{instr: instr, x: 1, cycle: 0, draw: ""}
+    |> loop(&draw/1)
+    |> Map.get(:draw)
+    |> String.codepoints()
+    |> Enum.chunk_every(40)
+    |> Enum.map(&Enum.join(&1, ""))
+    |> Enum.each(&IO.puts/1)
   end
 
-  def score_autocomplete([], value), do: value
-  def score_autocomplete(["(" | rest], value), do: score_autocomplete(rest, 5 * value + 1)
-  def score_autocomplete(["[" | rest], value), do: score_autocomplete(rest, 5 * value + 2)
-  def score_autocomplete(["{" | rest], value), do: score_autocomplete(rest, 5 * value + 3)
-  def score_autocomplete(["<" | rest], value), do: score_autocomplete(rest, 5 * value + 4)
+  defp draw(%{x: x, cycle: cycle, draw: draw} = machine)
+       when x - 1 <= rem(cycle - 1, 40) and rem(cycle - 1, 40) <= x + 1 do
+    %{machine | draw: draw <> "#"}
+  end
+
+  defp draw(%{draw: draw} = machine) do
+    %{machine | draw: draw <> "."}
+  end
 end
