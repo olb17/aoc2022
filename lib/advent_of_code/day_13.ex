f86f1aabf51f7f737dd3042b10e5193c17241782
Olivier Belhomme <2453684+olb17@users.noreply.github.com>
2022-12-16 16:48:56 +0000

    Day13

--- lib/advent_of_code/day_13.ex
+++ lib/advent_of_code/day_13.ex
@@ -1,73 +1,53 @@
 defmodule AdventOfCode.Day13 do
   def part1(args) do
-    [dots, instr] = String.split(args, "\n\n", trim: true)
-
-    dots =
-      dots
-      |> String.split("\n", trim: true)
-      |> Enum.map(fn line ->
-        [x, y] = String.split(line, ",")
-        {{String.to_integer(x), String.to_integer(y)}, "x"}
-      end)
-      |> Map.new()
-
-    ["fold along " <> dimension, fold] =
-      String.split(instr, "\n", trim: true)
-      |> hd
-      |> String.split("=")
-
-    fold(dimension, String.to_integer(fold), dots)
-    |> Enum.count()
+    args
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(fn pairs ->
+      [left, right] = pairs |> String.split("\n", trim: true)
+      {left |> parse_line(), right |> parse_line()}
+    end)
+    |> Enum.map(fn {left, right} -> compare(left, right) end)
+    |> Enum.with_index()
+    |> Enum.map(fn
+      {true, i} -> i + 1
+      {false, _i} -> 0
+    end)
+    |> Enum.sum()
   end
 
-  def fold("x", fold, dots) do
-    {unfolded, folded} = Enum.split_with(dots, fn {{x, _y}, _} -> x < fold end)
+  defp compare(a, a) when is_integer(a), do: true
+  defp compare([], []), do: nil
+  defp compare([_ | _], []), do: false
+  defp compare([], [_ | _]), do: true
+  defp compare(a, b) when is_integer(a) and is_integer(b), do: a < b
 
-    (unfolded ++ Enum.map(folded, fn {{x, y}, v} -> {{fold - (x - fold), y}, v} end))
-    |> Map.new()
+  defp compare(a, b) when is_integer(a) and is_list(b), do: compare([a], b)
+  defp compare(a, b) when is_integer(b) and is_list(a), do: compare(a, [b])
+  defp compare([a | resta], [a | restb]), do: compare(resta, restb)
+
+  defp compare([a | resta], [b | restb]) do
+    res = compare(a, b)
+    if res != nil, do: res, else: compare(resta, restb)
   end
 
-  def fold("y", fold, dots) do
-    {unfolded, folded} = Enum.split_with(dots, fn {{_x, y}, _} -> y < fold end)
-
-    (unfolded ++ Enum.map(folded, fn {{x, y}, v} -> {{x, fold - (y - fold)}, v} end))
-    |> Map.new()
-  end
-
+  @div1 [[2]]
+  @div2 [[6]]
   def part2(args) do
-    [dots_str, instr] = String.split(args, "\n\n", trim: true)
-
-    dots =
-      dots_str
-      |> String.split("\n", trim: true)
-      |> Enum.map(fn line ->
-        [x, y] = String.split(line, ",")
-        {{String.to_integer(x), String.to_integer(y)}, "x"}
-      end)
-      |> Map.new()
-
-    dots =
-      instr
-      |> String.split("\n", trim: true)
-      |> Enum.reduce(dots, fn instr, acc ->
-        ["fold along " <> dimension, fold] = String.split(instr, "=")
-        fold(dimension, String.to_integer(fold), acc)
-      end)
-
-    {minx, maxx} = Map.keys(dots) |> Enum.map(&elem(&1, 0)) |> Enum.min_max()
-
-    {miny, maxy} = Map.keys(dots) |> Enum.map(&elem(&1, 1)) |> Enum.min_max()
-
-    IO.puts("")
-
-    for y <- miny..maxy do
-      for x <- minx..maxx do
-        IO.write(Map.get(dots, {x, y}, " "))
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn str -> str |> String.trim() |> parse_line end)
+    |> Kernel.++([@div1, @div2])
+    |> Enum.sort_by(& &1, &compare/2)
+    |> Enum.with_index()
+    |> Enum.flat_map(fn {elt, i} ->
+      if elt == @div1 or elt == @div2 do
+        [i + 1]
+      else
+        []
       end
-
-      IO.puts("")
-    end
-
-    IO.puts("")
+    end)
+    |> Enum.product()
   end
+
+  def parse_line(str), do: Code.eval_string(str) |> elem(0)
 end
